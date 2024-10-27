5aba524c5d8b809b760d50af8e1949d00c05d77f
liss22 <liss22@hanmail.net>
2024-10-27 21:33:59 +0900

    v0.78.3

--- data/modules/update_user_metadata.lua
+++
@@ -1,8 +0,0 @@
-local nk = require("nakama")
-
-local function update_user_metadata(context, payload)
-    local json = nk.json_decode(payload)
-    nk.account_update_id(json.user_id, json.metadata)
-end
-
-nk.register_rpc(update_user_metadata, "update_user_metadata_fn")
