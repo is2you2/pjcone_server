0d08096c9706f4c9663efb61d20f5779132040a8
liss22 <liss22@hanmail.net>
2024-10-31 15:37:14 +0900

    v0.79.1

--- uploader/www/assets/p5js/zxing_scanner.js
+++ uploader/www/assets/p5js/zxing_scanner.js
@@ -1,17 +1,29 @@
-let capture;
-
 const codeReader = new ZXing.BrowserMultiFormatReader();
+var capture;
+var DeviceList = [];
+var DeviceIndex = 0;
 
 function setup() {
   noCanvas();
   capture = createCapture(VIDEO);
-  capture.elt.id = "qr_scanner";
+  capture.elt.id = 'qr_capture';
   capture.size(400, 368);
+  window['ChangeDevice'] = ChangeDevice;
   codeReader.listVideoInputDevices()
-    .then(videoInputDevices => {
-      const firstDeviceId = videoInputDevices[0].deviceId;
-      codeReader.decodeFromVideoDevice(firstDeviceId, 'qr_scanner', (result, err) => {
-        if (result) window['scan_result'](result);
-      })
+    .then(async videoInputDevices => {
+      DeviceList = videoInputDevices;
+      const firstDeviceId = videoInputDevices[DeviceIndex].deviceId;
+      codeReader.decodeFromVideoDevice(firstDeviceId, capture.elt, (result, err) => {
+        if (result && window['scan_result']) window['scan_result'](result);
+      });
     }).catch(err => console.error(err));
 }
+
+async function ChangeDevice() {
+  codeReader.reset();
+  DeviceIndex = (DeviceIndex + 1) % DeviceList.length;
+  const DeviceId = DeviceList[DeviceIndex].deviceId;
+  codeReader.decodeFromVideoDevice(DeviceId, capture.elt, (result, err) => {
+    if (result && window['scan_result']) window['scan_result'](result);
+  });
+}  
